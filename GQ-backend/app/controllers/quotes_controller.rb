6d91813d8120590d92cebd3de2ce8dbee5b73983
Mark <m.hern1415@gmail.com>
2020-03-10 17:41:37 -0700

    quote controller actions

--- GQ-backend/app/controllers/quotes_controller.rb
+++ GQ-backend/app/controllers/quotes_controller.rb
@@ -1,9 +1,23 @@
 class QuotesController < ApplicationController
 
-def index
-    @quotes = Quote.all 
+    def index
+        @quotes = Quote.all 
 
-    render json: @quotes, status:200
+        render json: @quotes, status: 200
+    end
+
+    def create 
+        @quote = Quote.new
+
+        render json: @quote, status: 200
+    end
+
+    def destroy 
+        @quote = Quote.find(params[:id])
+
+        render json: @quote, status: 200
+    end
+
 end
 
 
