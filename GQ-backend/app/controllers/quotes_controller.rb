7db88d256585c5759d95133def102dedb1f13aec
Mark <m.hern1415@gmail.com>
2020-03-12 12:57:13 -0700

    added controller actions

--- GQ-backend/app/controllers/quotes_controller.rb
+++ GQ-backend/app/controllers/quotes_controller.rb
@@ -1,2 +1,19 @@
 class QuotesController < ApplicationController
+    def index
+        @quotes = Quote.all 
+
+        render json: @quotes, status: 200
+    end
+
+    def create 
+        @quote = Quote.new(quote_params)
+
+        render json: @quote, status:200
+    end
+
+    def destroy
+        @quote = Quote.find(quote_params)
+        @quote.destroy
+        render json: ("Quote Deleted.").to_json 
+    end
 end
