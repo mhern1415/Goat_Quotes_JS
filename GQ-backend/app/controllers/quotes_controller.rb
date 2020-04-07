3971280fe815a94b3fa2ec0502d48cc57f9a36f4
Mark <m.hern1415@gmail.com>
2020-04-07 16:45:24 -0700

    added quote class

--- GQ-backend/app/controllers/quotes_controller.rb
+++ GQ-backend/app/controllers/quotes_controller.rb
@@ -8,13 +8,24 @@ class QuotesController < ApplicationController
 
     def create 
         @quote = Quote.new(content:params[:content], sport_id:params[:sport], athlete_id:(Athlete.create(name:params[:athlete])).id)  
-                if @quote.save
-                        render json: @quote.to_json(:include => {:athlete => {:only => [:name]} , :sport => {:only => [:name]}},
-                                :except => [:created_at, :updated_at])
-                else
+            if @quote.save
+                   render json: @quote.to_json(:include => {:athlete => {:only => [:name]} , :sport => {:only => [:name]}},
+                            :except => [:created_at, :updated_at])
+            else
                         render json: ("Error saving quote!").to_json
-                end
-        end
+            end
+    end
+
+    def update
+        @quote = Quote.find(params[:id])
+        @quote.update(content:params[:content], sport_id:params[:sport], athlete_id:(Athlete.create(name:params[:athlete])).id) 
+            if @quote.save
+                render json: @quote.to_json(:include => {:athlete => {:only => [:name]} , :sport => {:only => [:name]}},
+                    :except => [:created_at, :updated_at])
+            else
+                render json: ("Error saving quote!").to_json
+            end
+    end
 
     def destroy
         @quote = Quote.find(params[:id])
